68af683d062fb6cb621e8341ba8f03fc4cd6579a
KaramatjanovKabir <kabir_karamatjanov@mail.ru>
2022-07-01 13:38:12 +0500

    fnished velton project

---
+++ app.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById("burger");
+const Nav = document.querySelector(".ul_mobile");
+
+ burger.addEventListener("click", function() {
+    Nav.classList.toggle ("show_menu")
+});
+
+    
